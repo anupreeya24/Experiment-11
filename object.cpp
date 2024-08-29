e476b94b2389a1b6a01d345a48cd87c695607d81
anupreeya24 <anupreeyawo@gmail.com>
2024-08-29 09:47:33 +0530

    Create object.cpp

---
+++ object.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class student{
+    public:
+       void mystudentyear() {
+    cout<<"hello!, I am in 2nd year";
+       }
+   };
+   
+int main (){
+    student myobj;
+    myobj.mystudentyear();
+    return 0;
+}
+
+//hello!, I am in 2nd year
